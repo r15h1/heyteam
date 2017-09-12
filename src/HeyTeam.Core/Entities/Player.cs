7386a953041471c5b89917831b28d0a24ddc0ae5
r15h1 <tapsee.rishi@gmail.com>
2017-09-12 14:48:42 -0400

    player description tests

--- src/HeyTeam.Core/Entities/Player.cs
+++ src/HeyTeam.Core/Entities/Player.cs
@@ -2,9 +2,17 @@ namespace HeyTeam.Core.Entities
 {
     public class Player
     {
+        public enum Foot 
+        {
+            RIGHT,
+            LEFT
+        }
+
+        public long? Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Nationality { get;set; }
         public string SquadNumber {get; set; }
+        public Foot DominantFoot { get; set; } = Foot.RIGHT;
     }
 }
--- src/HeyTeam.Tests/PlayerTests.cs
+++ src/HeyTeam.Tests/PlayerTests.cs
@@ -10,15 +10,22 @@ namespace HeyTeam.Tests
         public void DescribePlayer()
         {
             Player player = new Player();
+            player.Id = 5;
             player.FirstName = "John";
             player.LastName = "Smith";
             player.Nationality = "Canadian";
             player.SquadNumber = "35";
+            player.DominantFoot = Player.Foot.LEFT;
             
+            Assert.Equal(player.Id, 5);
             Assert.Same(player.FirstName, "John");
             Assert.Same(player.LastName, "Smith");
             Assert.Same(player.Nationality, "Canadian");
             Assert.Same(player.SquadNumber, "35");
+            Assert.Equal(player.DominantFoot, Player.Foot.LEFT);             
         }
+
+        /*[Fact]
+        public void PlayerCanEvaluate*/
     }
 }
