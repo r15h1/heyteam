b7423cf3f2b9f669f9dee8ced9262d0f3326c976
r15h1 <tapsee.rishi@gmail.com>
2017-09-19 09:29:05 -0400

    #1 - adding Requests namespace

---
+++ src/HeyTeam.Core/Requests/GetClubRequest.cs
@@ -0,0 +1,19 @@
+using System.Collections.Generic;
+using HeyTeam.Core.Entities;
+using HeyTeam.Core.Validation;
+
+namespace HeyTeam.Core.Requests {
+    public class GetClubRequest {
+        public long? ClubId { get; set; }
+        public string NameStartsWith { get; set; }
+    }
+
+    public class GetClubResponse {
+        public GetClubResponse(ValidationResult<GetClubRequest> validationResult, IList<Club> clubs) {
+            Clubs = clubs;
+            ValidationResult = validationResult;
+        }
+        public IList<Club> Clubs { get; private set; }        
+        public ValidationResult<GetClubRequest> ValidationResult { get; private set; }
+    }
+}
---
+++ src/HeyTeam.Core/Requests/SaveClubRequest.cs
@@ -0,0 +1,17 @@
+using HeyTeam.Core.Validation;
+
+namespace HeyTeam.Core.Requests {
+    public class SaveClubRequest {
+        public long? ClubId { get; set; }
+        public string ClubName { get; set; }        
+    }
+
+    public class SaveClubResponse {
+        public SaveClubResponse(ValidationResult<SaveClubRequest> validationResult, long? clubId = null) {
+            ClubId = clubId;
+            ValidationResult = validationResult;
+        }
+        public long? ClubId { get; private set; }        
+        public ValidationResult<SaveClubRequest> ValidationResult { get; private set; }
+    }
+}
