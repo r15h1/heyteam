3a2dcee4b2270c2978a7f0f7912e7767f2993047
Rishi <tapsee.rishi@gmail.com>
2017-09-26 15:00:38 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -13,7 +13,7 @@ dotnet restore
 
 # Ideally we would use the 'dotnet test' command to test netcoreapp and net451 so restrict for now 
 # but this currently doesn't work due to https://github.com/dotnet/cli/issues/3073 so restrict to netcoreapp
-cp ./src/database/SqliteScript.sql .src/HeyTeam.Tests/bin/Debug/netcoreapp2.0/SqliteScript.sql
+cp ./src/database/SqliteScript.sql ./src/HeyTeam.Tests/bin/Debug/netcoreapp2.0/SqliteScript.sql
 dotnet test ./src/HeyTeam.Tests/HeyTeam.Tests.csproj -c Release -f netcoreapp2.0
 
 revision=${TRAVIS_JOB_ID:=1}  
