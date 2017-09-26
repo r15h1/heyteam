258d8e9c275e1cc1b5fe84237f19c5c0d3cdc20d
r15h1 <tapsee.rishi@gmail.com>
2017-09-26 14:27:38 -0400

    converted club update profile tests to use dapper

--- src/HeyTeam.Lib/Repositories/ClubRepository.cs
+++ src/HeyTeam.Lib/Repositories/ClubRepository.cs
@@ -21,7 +21,7 @@ namespace HeyTeam.Lib.Repositories {
             {
                 string sql = "INSERT INTO CLUBS(Guid, Name, LogoUrl) SELECT @Guid, @Name, @LogoUrl WHERE NOT EXISTS (SELECT 1 FROM CLUBS WHERE Guid = @Guid)";  
                 var p = new DynamicParameters();
-                p.Add("@Guid", club.Guid);
+                p.Add("@Guid", club.Guid.ToString());
                 p.Add("@Name", club.Name);
                 p.Add("@LogoUrl", club.LogoUrl);
                 connection.Open();
@@ -31,21 +31,33 @@ namespace HeyTeam.Lib.Repositories {
 
         public void Update(Core.Entities.Club club)
         {
-            // using(var context = new ClubContext(options)) {
-            //     var existingClub = context.Clubs.FirstOrDefault(c => c.Guid == club.Guid);
-            //     existingClub.LogoUrl = club.LogoUrl;
-            //     existingClub.Name = club.Name;    
-            //     context.SaveChanges();               
-            // }
+            using (var connection = connectionFactory.Connect())
+            {
+                string sql = "UPDATE CLUBS SET Name = @Name, LogoUrl = @LogoUrl WHERE Guid = @Guid";  
+                var p = new DynamicParameters();
+                p.Add("@Guid", club.Guid.ToString());
+                p.Add("@Name", club.Name);
+                p.Add("@LogoUrl", club.LogoUrl);
+                connection.Open();
+                connection.Execute(sql, p);
+            }
         }
 
-        public Core.Entities.Club Get(Guid clubId)
+        public Club Get(Guid clubId)
         {     
-            // using(var context = new ClubContext(options)) {          
-            //     var club = context.Clubs.Include(c => c.Squads).FirstOrDefault(c => c.Guid == clubId);
-            //     return mapper.Map<Core.Entities.Club>(club);            
-            // }
-            throw new NotImplementedException();
+            using (var connection = connectionFactory.Connect())
+            {
+                string sql = "SELECT Guid, Name, LogoUrl FROM Clubs WHERE Guid = @Guid";  
+                var p = new DynamicParameters();
+                p.Add("@Guid", clubId.ToString());
+                connection.Open();
+                var results = connection.Query(sql, p).Cast<IDictionary<string, object>>();
+                return results.Select(row => 
+                        new Club(Guid.Parse(row["Guid"].ToString())){ 
+                            Name = (string)row["Name"], 
+                            LogoUrl = (string)row["LogoUrl"]}
+                        ).FirstOrDefault();
+            }
         }
 
         public IList<Core.Entities.Club> Get(string nameStartsWith)
--- src/HeyTeam.Tests/UseCases/UpdateClubProfileTests.cs
+++ src/HeyTeam.Tests/UseCases/UpdateClubProfileTests.cs
@@ -7,113 +7,116 @@ using HeyTeam.Core.Validation;
 using HeyTeam.Lib.Data;
 using HeyTeam.Lib.Repositories;
 using HeyTeam.Lib.Validation;
+using HeyTeam.Tests.Data;
 using Xunit;
 
 namespace HeyTeam.Tests.UseCases {
     public class UpdateClubProfileTests {
-        // private readonly IClubRepository repository;
-        // private readonly IUseCase<UpdateClubProfileRequest, UpdateClubProfileResponse> updateProfileUseCase;
-        // private readonly Guid clubId;
+        private readonly IClubRepository repository;
+        private readonly IUseCase<UpdateClubProfileRequest, UpdateClubProfileResponse> updateProfileUseCase;
+        private readonly Guid clubId;
 
-        // public UpdateClubProfileTests() {            
-        //     IValidator<UpdateClubProfileRequest> validator = new UpdateClubProfileRequestValidator();
-        //     this.repository = new ClubRepository(null);
-        //     this.updateProfileUseCase = new UpdateClubProfileUseCase(repository, validator);
-        //     this.clubId = SetupClub();
-        // }
+        public UpdateClubProfileTests() {
+            string connectionString = $"Data Source=file:{Guid.NewGuid().ToString()}.sqlite";
+            Database.Create(connectionString);                     
+            IValidator<UpdateClubProfileRequest> validator = new UpdateClubProfileRequestValidator();
+            this.repository = new ClubRepository(new ConnectionFactory(connectionString));
+            this.updateProfileUseCase = new UpdateClubProfileUseCase(repository, validator);
+            this.clubId = SetupClub();
+        }
 
-        // private Guid SetupClub() {
-        //     var registerUseCase = new RegisterClubUseCase(repository, new RegisterClubRequestValidator());
-        //     RegisterClubRequest registerRequest = new RegisterClubRequest { ClubName = "Manchester United" , ClubLogoUrl = "http://manutd.com"};
-        //     var registerResponse = registerUseCase.Execute(registerRequest);  
-        //     return registerResponse.ClubId.Value;
-        // }
+        private Guid SetupClub() {
+            var registerUseCase = new RegisterClubUseCase(repository, new RegisterClubRequestValidator());
+            RegisterClubRequest registerRequest = new RegisterClubRequest { ClubName = "Manchester United" , ClubLogoUrl = "http://manutd.com"};
+            var registerResponse = registerUseCase.Execute(registerRequest);  
+            return registerResponse.ClubId.Value;
+        }
 
-        // [Fact]
-        // public void ClubIdMustBeAssigned() {
-        //     UpdateClubProfileRequest request = new UpdateClubProfileRequest { ClubName = "Valid name" };
-        //     var response = updateProfileUseCase.Execute(request);            
-        //     Assert.True(!response.ValidationResult.IsValid && response.ValidationResult.Messages.Count == 1);
-        // }
+        [Fact]
+        public void ClubIdMustBeAssigned() {
+            UpdateClubProfileRequest request = new UpdateClubProfileRequest { ClubName = "Valid name" };
+            var response = updateProfileUseCase.Execute(request);            
+            Assert.True(!response.ValidationResult.IsValid && response.ValidationResult.Messages.Count == 1);
+        }
 
-        // [Fact]
-        // public void InexistentClubUpdateThrowsClubNotFoundException() {
-        //     UpdateClubProfileRequest request = new UpdateClubProfileRequest { ClubId = Guid.NewGuid(), ClubName = "Manchester United" , ClubLogoUrl = "https://google.com"};            
-        //     Assert.Throws<ClubNotFoundException>(
-        //         () => updateProfileUseCase.Execute(request)
-        //     );
-        // }
+        [Fact]
+        public void InexistentClubUpdateThrowsClubNotFoundException() {
+            UpdateClubProfileRequest request = new UpdateClubProfileRequest { ClubId = Guid.NewGuid(), ClubName = "Manchester United" , ClubLogoUrl = "https://google.com"};            
+            Assert.Throws<ClubNotFoundException>(
+                () => updateProfileUseCase.Execute(request)
+            );
+        }
 
-        // [Fact]
-        // public void ClubNameCannotBeNull() {
-        //     UpdateClubProfileRequest request = new UpdateClubProfileRequest { ClubId = clubId, ClubName = null };
-        //     var response = updateProfileUseCase.Execute(request);            
-        //     Assert.True(!response.ValidationResult.IsValid && response.ValidationResult.Messages.Count == 1);
-        // }
+        [Fact]
+        public void ClubNameCannotBeNull() {
+            UpdateClubProfileRequest request = new UpdateClubProfileRequest { ClubId = clubId, ClubName = null };
+            var response = updateProfileUseCase.Execute(request);            
+            Assert.True(!response.ValidationResult.IsValid && response.ValidationResult.Messages.Count == 1);
+        }
 
-        // [Fact]
-        // public void ClubNameCannotBeEmpty() {
-        //     UpdateClubProfileRequest request = new UpdateClubProfileRequest { ClubId = clubId, ClubName = string.Empty };
-        //     var response = updateProfileUseCase.Execute(request);            
-        //     Assert.True(!response.ValidationResult.IsValid && response.ValidationResult.Messages.Count == 1);
-        // }
+        [Fact]
+        public void ClubNameCannotBeEmpty() {
+            UpdateClubProfileRequest request = new UpdateClubProfileRequest { ClubId = clubId, ClubName = string.Empty };
+            var response = updateProfileUseCase.Execute(request);            
+            Assert.True(!response.ValidationResult.IsValid && response.ValidationResult.Messages.Count == 1);
+        }
 
-        // [Fact]
-        // public void ClubNameCannotBeWhiteSpace() {
-        //     UpdateClubProfileRequest request = new UpdateClubProfileRequest { ClubId = clubId, ClubName = "  " };
-        //     var response = updateProfileUseCase.Execute(request);            
-        //     Assert.True(!response.ValidationResult.IsValid && response.ValidationResult.Messages.Count == 1);
-        // }
+        [Fact]
+        public void ClubNameCannotBeWhiteSpace() {
+            UpdateClubProfileRequest request = new UpdateClubProfileRequest { ClubId = clubId, ClubName = "  " };
+            var response = updateProfileUseCase.Execute(request);            
+            Assert.True(!response.ValidationResult.IsValid && response.ValidationResult.Messages.Count == 1);
+        }
 
-        // [Fact]
-        // public void LogoUrlRequiresScheme() {
-        //     UpdateClubProfileRequest updateRequest = new UpdateClubProfileRequest { ClubId = clubId,  ClubName = "Manchester United" , ClubLogoUrl = "google.com"};
-        //     var response = updateProfileUseCase.Execute(updateRequest);            
-        //     Assert.True(!response.ValidationResult.IsValid && response.ValidationResult.Messages.Count == 1);
-        // }
+        [Fact]
+        public void LogoUrlRequiresScheme() {
+            UpdateClubProfileRequest updateRequest = new UpdateClubProfileRequest { ClubId = clubId,  ClubName = "Manchester United" , ClubLogoUrl = "google.com"};
+            var response = updateProfileUseCase.Execute(updateRequest);            
+            Assert.True(!response.ValidationResult.IsValid && response.ValidationResult.Messages.Count == 1);
+        }
         
-        // [Fact]
-        // public void NameUpdateIsSaved()
-        // {
-        //     UpdateClubProfileRequest request = new UpdateClubProfileRequest { ClubId = clubId, ClubName = "Barcelona", ClubLogoUrl = "http://manutd.com" };
-        //     var response = updateProfileUseCase.Execute(request);
-        //     var club = repository.Get(clubId);
-        //     Assert.True(response.ValidationResult.IsValid);
-        //     CheckNameAndLogo(club, "Barcelona", "http://manutd.com");
-        // }
+        [Fact]
+        public void NameUpdateIsSaved()
+        {
+            UpdateClubProfileRequest request = new UpdateClubProfileRequest { ClubId = clubId, ClubName = "Barcelona", ClubLogoUrl = "http://manutd.com" };
+            var response = updateProfileUseCase.Execute(request);
+            var club = repository.Get(clubId);
+            Assert.True(response.ValidationResult.IsValid);
+            CheckNameAndLogo(club, "Barcelona", "http://manutd.com");
+        }
 
-        // [Fact]
-        // public void HttpLogoUrlUpdateIsSaved()
-        // {
-        //     UpdateClubProfileRequest request = new UpdateClubProfileRequest { ClubId = clubId, ClubName = "Manchester United", ClubLogoUrl = "http://google.com" };
-        //     var response = updateProfileUseCase.Execute(request);
-        //     var club = repository.Get(clubId);
-        //     Assert.True(response.ValidationResult.IsValid);
-        //     CheckNameAndLogo(club, "Manchester United", "http://google.com");
-        // }
+        [Fact]
+        public void HttpLogoUrlUpdateIsSaved()
+        {
+            UpdateClubProfileRequest request = new UpdateClubProfileRequest { ClubId = clubId, ClubName = "Manchester United", ClubLogoUrl = "http://google.com" };
+            var response = updateProfileUseCase.Execute(request);
+            var club = repository.Get(clubId);
+            Assert.True(response.ValidationResult.IsValid);
+            CheckNameAndLogo(club, "Manchester United", "http://google.com");
+        }
 
-        // [Fact]
-        // public void HttpsLogoUrlIsSaved() {
-        //     UpdateClubProfileRequest request = new UpdateClubProfileRequest { ClubId = clubId, ClubName = "Manchester United" , ClubLogoUrl = "https://google.com"};
-        //     var response = updateProfileUseCase.Execute(request);  
-        //     var club = repository.Get(clubId);          
-        //     Assert.True(response.ValidationResult.IsValid);
-        //     CheckNameAndLogo(club, "Manchester United", "https://google.com");
-        // }
+        [Fact]
+        public void HttpsLogoUrlIsSaved() {
+            UpdateClubProfileRequest request = new UpdateClubProfileRequest { ClubId = clubId, ClubName = "Manchester United" , ClubLogoUrl = "https://google.com"};
+            var response = updateProfileUseCase.Execute(request);  
+            var club = repository.Get(clubId);          
+            Assert.True(response.ValidationResult.IsValid);
+            CheckNameAndLogo(club, "Manchester United", "https://google.com");
+        }
 
-        // [Fact]
-        // public void BothNameAndLogoAreUpdated() {
-        //     UpdateClubProfileRequest request = new UpdateClubProfileRequest { ClubId = clubId, ClubName = "Barcelona" , ClubLogoUrl = "https://www.fcbarcelona.com"};
-        //     var response = updateProfileUseCase.Execute(request);  
-        //     var club = repository.Get(clubId);          
-        //     Assert.True(response.ValidationResult.IsValid);
-        //     CheckNameAndLogo(club, "Barcelona", "https://www.fcbarcelona.com");
-        // }
+        [Fact]
+        public void BothNameAndLogoAreUpdated() {
+            UpdateClubProfileRequest request = new UpdateClubProfileRequest { ClubId = clubId, ClubName = "Barcelona" , ClubLogoUrl = "https://www.fcbarcelona.com"};
+            var response = updateProfileUseCase.Execute(request);  
+            var club = repository.Get(clubId);          
+            Assert.True(response.ValidationResult.IsValid);
+            CheckNameAndLogo(club, "Barcelona", "https://www.fcbarcelona.com");
+        }
 
-        // private void CheckNameAndLogo(Core.Entities.Club club, string name, string logoUrl)
-        // {
-        //     Assert.True(club.LogoUrl.Equals(logoUrl));
-        //     Assert.True(club.Name.Equals(name));
-        // }
+        private void CheckNameAndLogo(Core.Entities.Club club, string name, string logoUrl)
+        {
+            Assert.True(club.LogoUrl.Equals(logoUrl));
+            Assert.True(club.Name.Equals(name));
+        }
     }    
 }
