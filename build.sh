0f45af21bbe5495c8514b3a2a1f2a209ecfa4651
Rishi <tapsee.rishi@gmail.com>
2017-09-27 10:28:22 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -13,7 +13,10 @@ dotnet restore
 
 # Ideally we would use the 'dotnet test' command to test netcoreapp and net451 so restrict for now 
 # but this currently doesn't work due to https://github.com/dotnet/cli/issues/3073 so restrict to netcoreapp
+mkdir /home/travis/build/r15h1/heyteam/src/HeyTeam.Tests/bin
+mkdir /home/travis/build/r15h1/heyteam/src/HeyTeam.Tests/bin/Release
 mkdir /home/travis/build/r15h1/heyteam/src/HeyTeam.Tests/bin/Release/netcoreapp2.0
+
 cp ./src/database/SqliteScript.sql /home/travis/build/r15h1/heyteam/src/HeyTeam.Tests/bin/Release/netcoreapp2.0/
 dotnet test ./src/HeyTeam.Tests/HeyTeam.Tests.csproj -c Release -f netcoreapp2.0
 
