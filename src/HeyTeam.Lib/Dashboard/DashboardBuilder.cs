b979fe770e568bf344810b301a3e46594c3253a4
r15h1 <tapsee.rishi@gmail.com>
2017-10-17 14:27:28 -0400

    #12- added test assets for dashboard use case

---
+++ src/HeyTeam.Lib/Dashboard/DashboardBuilder.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using HeyTeam.Core.Dashboard;
+using HeyTeam.Core.Repositories;
+using HeyTeam.Identity;
+
+namespace HeyTeam.Lib.Dashboard {
+    public class DashboardBuilder : AbstractDashboardBuilder
+    {
+        public DashboardBuilder(IDashboardRepository dashboardRepository, IUserRepository userRepository) : base(dashboardRepository, userRepository)
+        {
+        }
+
+        public override List<Group> Build()
+        {       
+            List<Group> dashboard = new List<Group>();     
+            var roles = userRepository.GetRoles(base.UserEmail);
+            if(roles == null || roles.Count() == 0) return dashboard;
+
+            if(roles.Contains(Roles.Administrator.ToString()) || roles.Contains(Roles.Coach.ToString()))
+                dashboard.Add(GetSquadSummary(UserEmail));
+
+            if(roles.Contains(Roles.Player.ToString()) || roles.Contains(Roles.Coach.ToString()))
+                dashboard.Add(GetActionItems(UserEmail));
+
+            dashboard.Add(GetUpcomingEvents(UserEmail));
+            dashboard.Add(GetActivityLog(UserEmail));
+            
+            return dashboard;
+        }
+
+        private Group GetSquadSummary(string userEmail)
+        {
+            throw new NotImplementedException();
+        }
+
+        private Group GetActionItems(string userEmail)
+        {
+            throw new NotImplementedException();
+        }
+        
+        private Group GetUpcomingEvents(string userEmail)
+        {
+            throw new NotImplementedException();
+        }
+
+        private Group GetActivityLog(string userEmail)
+        {
+            throw new NotImplementedException();
+        }        
+    }
+}
