6be6f38c31757f86280f9c6b510f33bf2a62b4d9
r15h1 <tapsee.rishi@gmail.com>
2017-11-01 16:11:26 -0400

    #6 - create player details ui

--- src/HeyTeam.Web/Controllers/PlayersController.cs
+++ src/HeyTeam.Web/Controllers/PlayersController.cs
@@ -6,8 +6,7 @@ using HeyTeam.Web.Models.PlayerViewModels;
 namespace HeyTeam.Web.Controllers
 {
 
-    [Authorize]
-    [Route("squads/{squadId:guid}/[controller]")]
+    [Authorize]    
     public class PlayersController : Controller {
         private readonly IPlayerRepository playerRepository;
 
@@ -15,13 +14,15 @@ namespace HeyTeam.Web.Controllers
             this.playerRepository = playerRepository;
         }
 
-        [HttpGet("new")]
+        [Route("squads/{squadId:guid}/[controller]/new")]
+        [HttpGet]
         public IActionResult New() {
             ViewData["Title"] = "Add New Player";
             return View("Edit");
         }
 
-        [HttpGet("{playerId:guid}")]
+        [Route("players/{playerId:guid}")]
+        [HttpGet]
         public IActionResult Details(string squadId, string playerId) {
             var player = playerRepository.GetPlayer(System.Guid.Parse(playerId));
             PlayerDetailsViewModel model = new PlayerDetailsViewModel {
