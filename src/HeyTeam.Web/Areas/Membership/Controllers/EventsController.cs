3746f73e109dbaa2fabcd127417240174e88b83d
r15h1 <tapsee.rishi@gmail.com>
2017-12-22 13:36:25 -0500

    #51 added controller and landing page for membership area events

---
+++ src/HeyTeam.Web/Areas/Membership/Controllers/EventsController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+
+namespace HeyTeam.Web.Areas.Membership.Controllers
+{
+	[Authorize(Policy = "PlayerOrCoach")]
+	[Area("Membership")]
+	[Route("[area]/{memberid:guid}/[controller]")]
+	public class EventsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
