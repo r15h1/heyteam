542acb97708a0f0c7699fa13c406739de83dc67a
r15h1 <tapsee.rishi@gmail.com>
2018-04-01 13:43:17 -0400

    #79 - create assignment views and viewmodel

---
+++ src/HeyTeam.Web/Areas/Administration/Controllers/AssignmentsController.cs
@@ -0,0 +1,47 @@
+﻿using HeyTeam.Core;
+using HeyTeam.Core.Queries;
+using HeyTeam.Web.Models.Assignments;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HeyTeam.Web.Areas.Administration.Controllers
+{
+    [Authorize(Policy = "Administrator")]
+    [Area("Administration")]
+    [Route("[area]/[controller]")]
+    public class AssignmentsController : Controller
+    {
+        private readonly Club club;
+        private readonly ISquadQuery squadQuery;
+
+        public AssignmentsController(Club club, ISquadQuery squadQuery)
+        {
+            this.club = club;
+            this.squadQuery = squadQuery;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet("new")]
+        public IActionResult New()
+        {
+            var model = new AssignmentDetailsViewModel { SquadList = GetSquadList() };
+            return View("Create", model);
+        }
+
+        private List<SelectListItem> GetSquadList()
+        {
+            var clubSquads = squadQuery.GetSquads(club.Guid);
+            var squadList = clubSquads.Select(s => new SelectListItem { Text = $"{s.Name}", Value = s.Guid.ToString() })
+                                    .OrderBy(s => s.Text)
+                                    .ToList();
+            return squadList;
+        }
+    }
+}
---
+++ src/HeyTeam.Web/Models/Assignments/AssignmentDetailsViewModel.cs
@@ -0,0 +1,32 @@
+﻿using HeyTeam.Core;
+using HeyTeam.Core.Models.Mini;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HeyTeam.Web.Models.Assignments
+{
+    public class AssignmentDetailsViewModel
+    {
+        public Guid? Guid { get; set; }
+        public DateTime? SubmittedOn { get; set; }
+
+        [Required]
+        public int CoachId { get; set; }
+
+        public DateTime? DateDue { get; set; }
+
+        [Required]
+        public string Notes { get; set; }
+
+        public IEnumerable<TrainingMaterial> TrainingMaterials { get; set; }
+
+        public IEnumerable<MiniModel> Squads { get; set; }
+
+        public IEnumerable<MiniModel> Players { get; set; }
+        public IEnumerable<SelectListItem> SquadList { get; internal set; }
+    }
+}
