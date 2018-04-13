4af3e88d2c85bb6990c48317c4e2ebc97e0a4abc
Rishi Tapsee <Rishi.Tapsee@ypnexthome.ca>
2018-04-13 11:58:16 -0400

    #81 - adding feedback controller for player

---
+++ src/HeyTeam.Web/Areas/Players/Controllers/FeedbackController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HeyTeam.Web.Areas.Players.Controllers
+{
+	[Authorize(Policy = "Player")]
+	[Area("Players")]
+	[Route("[area]/{memberid:guid}")]
+	[Route("[area]/{memberid:guid}/[controller]")]
+	public class FeedbackController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
