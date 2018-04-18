882969b4dc281f5df14e79ff6fef57973fe2ab5e
r15h1 <tapsee.rishi@gmail.com>
2018-04-18 11:13:12 -0400

    #79 - parse and display due date in correct format

--- src/HeyTeam.Web/wwwroot/js/site.js
+++ src/HeyTeam.Web/wwwroot/js/site.js
@@ -1,1 +1,21 @@
 ﻿// Write your JavaScript code.
+function formatDate(date) {
+    var monthNames = [
+        "Jan", "Feb", "Mar",
+        "Apr", "May", "Jun", "Jul",
+        "Aug", "Sep", "Oct",
+        "Nov", "Dec"
+    ];
+
+    var day = date.getDate();
+    var monthIndex = date.getMonth();
+    var year = date.getFullYear();
+
+    return day + '-' + monthNames[monthIndex] + '-' + year;
+}
+
+function parseDate(input) {
+    var parts = input.match(/(\d+)/g);
+    // new Date(year, month [, date [, hours[, minutes[, seconds[, ms]]]]])
+    return new Date(parts[0], parts[1] - 1, parts[2]); // months are 0-based
+}
